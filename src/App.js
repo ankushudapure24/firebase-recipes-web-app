72a45161746683113b6522706cae9476b76fd392
ankushudapure24 <ankush.udapure@kaliba.in>
2025-02-12 12:40:07 +0530

    commit after section 1 complete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import firebase from "./FirebaseConfig";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Firebase Recipes </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
